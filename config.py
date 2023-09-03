e53cfa67ff93cd57469872579dcb40f79c22a4b1
Khhhgh <103472630+Khhhgh@users.noreply.github.com>
2023-09-03 13:17:52 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+
+from pyrogram import enums
+API_ID = 24209135
+API_HASH = '9ad05c7abeb3fd58effd1328bb2af596'
+
+TOKEN = '6544840789:AAGhSIt-KF3EePHp2GJqk0bzP8k5mIi7JY4'
+BOT_ID = TOKEN.split(':')[0]
+SUDO = 1310488710
